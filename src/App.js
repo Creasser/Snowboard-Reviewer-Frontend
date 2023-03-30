50c962f3f6ecfe554ecf3faf0c6e2d78d9c306c9
Blake Creasser <blakecreasser@aggienetwork.com>
2023-03-30 16:15:12 -0500

    add snowboard component, set up initial snowboard state and fetch request

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import React from "react";
+import { useState, useEffect } from "react";
 
 function App() {
+
+  const [snowboards, setSnowboards] = useState([])
+
+  useEffect(() => {
+    fetch("http://localhost:9292/snowboards")
+    .then(r => r.json())
+    .then(data => setSnowboards(data))
+  })
+
+  const boardsToDisplay = snowboards.map(board => {
+    <h1></h1>
+  })
+
   return (
     <div className="App">
-     Hello World!
+     
     </div>
   );
 }
---
+++ src/Snowboard.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Snowboard({ id, brand, price, preferred_riding, directional }){
+    return (
+        <div>
+            
+        </div>
+    )
+}
