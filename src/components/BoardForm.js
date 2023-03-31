8cc2832d1c5374736e2b429079df39ee93253ad2
Blake Creasser <blakecreasser@aggienetwork.com>
2023-03-30 22:56:05 -0500

    minor style changes, start building new board form

---
+++ src/components/BoardForm.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function BoardForm(){
+    return (
+        <div>
+            
+        </div>
+    )
+}
--- src/components/Snowboard.js
+++ src/components/Snowboard.js
@@ -6,7 +6,7 @@ function Snowboard({ id, brand, price, preferredRiding, directional,img }){
     
         <div className="snowboardCard">
             <h1>{brand}</h1>
-            <img src={img}></img>
+            <img src={img} className='boardImage'></img>
             <h2>{price}</h2>
             <p>{preferredRiding}</p>
             <p>{directional ? 'Directional' : 'Twin'}</p>
@@ -17,3 +17,9 @@ function Snowboard({ id, brand, price, preferredRiding, directional,img }){
 }
 
 export default Snowboard
+
+
+// when the edit button is clicked, I want to send the user to another page that will allow them to update the info on the board. This should be able to be done with react-router
+//Send the post request from that page to update the board information, and it will persist through the backend
+//One submit the user will be redirected to the home page
+//add another button that will allow the user to leave a review of the board
