d9fc5541dd83015fade2608bef99c49da605a33d
Blake Creasser <blakecreasser@aggienetwork.com>
2023-04-03 16:16:38 -0500

    make default values of the form the intial value of the board being edited

--- src/components/Snowboard.js
+++ src/components/Snowboard.js
@@ -14,16 +14,12 @@ function Snowboard({ id, brand, price, preferredRiding, directional, img, onBoar
         onBoardDelete(id)
     }
 
-    function handleUpdate(){
-
-    }
-
 
     return (
 
             <div className="snowboardCard">
                 <h1>{brand}</h1>
-                <img src={img} className='boardImage'></img>
+                <img src={img} alt="Snowboard" className='boardImage'></img>
                 <h2>{price}</h2>
                 <p>{preferredRiding}</p>
                 <p>{directional ? 'Directional' : 'Twin'}</p>
--- src/components/UpdateForm.js
+++ src/components/UpdateForm.js
@@ -1,17 +1,86 @@
-import React from "react";
+import React, { useState } from "react";
 import { useParams } from "react-router-dom";
 
 function UpdateForm({ snowboards }){
-
-
 let {board_id} = useParams()
-
 const currentBoard = snowboards.find((board) => board.id == board_id)
+
+const [updatedBoard, setUpdatedBoard] = useState({
+    brand: currentBoard.brand,
+    price: currentBoard.price,
+    preferred_riding: currentBoard.preferred_riding,
+    directional: currentBoard.directional,
+    image_url: currentBoard.image_url
+})
+
+
+
+function handleChange(e){
+    const name = e.target.name
+    let value = e.target.value
+
+    if (e.target.type === "checkbox") {
+        value = e.target.checked
+    }
+
+    setUpdatedBoard({
+        ...updatedBoard,
+        [name]: value
+    })
+
+}
+
+function handleSubmit(e){
+    e.preventDefault()
+    console.log(updatedBoard)
+}
 
 
     return (
         <div>
           <div>{`You are updating the ${currentBoard.brand} ${currentBoard.preferred_riding} board!`}</div>
+          <form onSubmit={handleSubmit}>
+          <input
+                    type='text'
+                    name='brand'
+                    value={updatedBoard.brand}
+                    placeholder='Enter Snowboard Brand'
+                    onChange={handleChange}
+                ></input>
+                <input
+                    type='text'
+                    name='price'
+                    value={updatedBoard.price}
+                    placeholder='Enter Snowboard price'
+                    onChange={handleChange}
+                ></input>
+               <select name="preferred_riding" onChange={handleChange}>
+                    <option value='Mountain'>Mountain</option>
+                    <option value='Park'>Park</option>
+                    <option value='Back Country'>Back Country</option>
+                    <option value='Universal'>Universal</option>
+               </select>
+                <input
+                    type='text'
+                    name='image_url'
+                    value={updatedBoard.image_url}
+                    placeholder='Enter Snowboard image url'
+                    onChange={handleChange}
+                ></input>
+                <input
+                    type='checkbox'
+                    name='directional'
+                    id="directional"
+                    value={updatedBoard.directional}
+                    onChange={handleChange}
+                ></input>
+                <label>Directional?</label>
+                <input
+                    type='submit'
+                    name='submit'
+                    value='Add Board'
+                ></input>
+          </form>
         </div>
     )
 }
